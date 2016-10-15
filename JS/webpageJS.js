255f9552f9ce637703085cb2d204fcb687d22d9b
udaycoder <udayanbaidya@gmail.com>
2016-10-16 01:12:29 +0530

    Added media queries

--- JS/webpageJS.js
+++ JS/webpageJS.js
@@ -1,18 +1,24 @@
 jQuery(document).ready(function($){
 
 		$(window).on('scroll', function() {
+
 				var y_scroll_pos = window.pageYOffset;
-				var scroll_beg = 650;
+				var scroll_beg = 560;
         var scroll_end=900;
-        if(y_scroll_pos < scroll_beg){
+				var window_width=$(window).width();
+        if(y_scroll_pos < scroll_beg && window_width>=1186){
            $("#daily-quotes").fadeOut("slow");
         }
-				if(y_scroll_pos > scroll_beg && y_scroll_pos < scroll_end) {
+				if(y_scroll_pos > scroll_beg && y_scroll_pos < scroll_end && window_width>=1186) {
                $("#daily-quotes").fadeIn("slow");
 				}
-        if(y_scroll_pos > scroll_end) {
+        if(y_scroll_pos > scroll_end && window_width>=1186) {
                $("#daily-quotes").fadeOut();
         }
+				if(window_width < 1186){
+					 $("#daily-quotes").hide();
+				}
+
 		});
 
 	})
