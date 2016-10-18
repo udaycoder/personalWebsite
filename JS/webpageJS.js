1ccea8ca3c36a0a00e6cbfb4b36dcc16ba945c34
udaycoder <udayanbaidya@gmail.com>
2016-10-18 20:53:02 +0530

    Fixed quotes animation bugs

--- JS/webpageJS.js
+++ JS/webpageJS.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($){
 		$(window).on('scroll', function() {
 
 				var y_scroll_pos = window.pageYOffset;
-				var scroll_beg = 560;
+				var scroll_beg = 420;
         var scroll_end=900;
 				var window_width=$(window).width();
         if(y_scroll_pos < scroll_beg && window_width>=1186){
